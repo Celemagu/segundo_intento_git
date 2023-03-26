006a58bc29a3c4829503adccf851999dc9327dcb
Cesar Mateus <cesarmateus6@gmail.com>
2023-03-26 15:21:21 -0500

    Segundo intento

---
+++ src/Segundo_Intento.java
@@ -0,0 +1,13 @@
+
+public class Segundo_Intento {
+	public static void main (String[] args) {
+		//Prueba Cesar GIT 2 neo
+		System.out.println("mi primer commmit");
+		
+		String nombre= "Cesar Mateus";
+		
+		System.out.println(nombre);
+		
+		System.out.println("Lo logré");
+			}
+}
